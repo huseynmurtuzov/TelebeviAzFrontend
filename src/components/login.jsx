c24513d229407aec9d48ef4a4e39810aa5e6cfa5
huseynmurtuzov <murtuzovhuseyn2@gmail.com>
2025-08-27 00:36:18 +0400

    debug test

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -43,6 +43,7 @@ export default function Login() {
 
 
   const handleLoginFunction = async (formData) => {
+    console.log("API URL:", process.env.REACT_APP_API_URL);
   return await api.post("/Account/login", {
     email:formData.email,
     password:formData.password
