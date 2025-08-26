22761a1c71378460bf9aeba7fa3171651cbcb50a
huseynmurtuzov <murtuzovhuseyn2@gmail.com>
2025-08-26 23:30:34 +0400

    .env added

--- src/api.jsx
+++ src/api.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7107/api",
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
 });
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
